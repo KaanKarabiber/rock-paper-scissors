0c8a85259df8e05ee1a7560c2a3fe10f7086f0eb
kaankarabiber <kaankarabiber1@gmail.com>
2023-08-24 13:19:30 +0300

    add getComputerChoice function it returns scissors, rock and paper at random

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-console.log("hello")
+function getComputerChoice(){
+    randomNumber = Math.random()
+    if (randomNumber <= 0.334){
+        return "Scissors"
+    }
+    else if (randomNumber >= 0.334 && randomNumber <= 0.667){
+        return "Rock";
+    }
+    else{
+        return "Paper";
+    }  
+}
+
+computerSelection = getComputerChoice();
+console.log(computerSelection)
