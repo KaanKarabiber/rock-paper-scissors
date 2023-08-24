4b2e9703f90f72d982771eaa500463f7145c005f
kaankarabiber <kaankarabiber1@gmail.com>
2023-08-24 13:36:00 +0300

    add singleRound function it plays a single round of rock paper scissors

--- code.js
+++ code.js
@@ -1,15 +1,38 @@
 function getComputerChoice(){
     randomNumber = Math.random()
     if (randomNumber <= 0.334){
-        return "Scissors"
+        return "scissors"
     }
     else if (randomNumber >= 0.334 && randomNumber <= 0.667){
-        return "Rock";
+        return "rock";
     }
     else{
-        return "Paper";
+        return "paper";
     }  
 }
-
-computerSelection = getComputerChoice();
-console.log(computerSelection)
+function singleRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "Tie"
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! rock beats scissors"
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! scissors beats paper"
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! paper beats rock"
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! rock beats scissors"
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! scissors beats paper"
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose! paper beats rock"
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(singleRound(playerSelection, computerSelection))
